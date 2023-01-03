1301d539bf513963be7b1e3ab6fd282e415faf70
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-01-02 22:36:36 -0600

    Login Form visual

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -7,8 +8,93 @@ class LoginScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: AuthBackground()
+    return  Scaffold(
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children:  [
+
+              const SizedBox( height: 250, ),
+
+              CardContainer(
+                child: Column(
+                  children: [
+
+                    const SizedBox( height: 10, ),
+                    Text('Login', style: Theme.of(context).textTheme.headline4,),
+                    const SizedBox(height: 30,),
+
+                    const _LofinForm()
+
+                  ],
+                ),
+              ),
+            
+              const SizedBox(height: 50,),
+              const Text('Crear una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+
+              const SizedBox(height: 50,),
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
+
+
+class _LofinForm extends StatelessWidget {
+  const _LofinForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        // mantener la referencia al KEY
+        child:  Column(
+          children: [
+
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'johndoe@gmail.com',
+                labelText: 'Correo electrónico',
+                prefixIcon: Icons.alternate_email_rounded
+              ),
+            ),
+
+            const SizedBox( height: 30, ),
+
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '******',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outline
+              ),
+            ),
+
+            const SizedBox( height: 30, ),
+
+            MaterialButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: const EdgeInsets.symmetric( horizontal: 80, vertical: 15),
+                child: const Text('Ingresar', style: TextStyle( color: Colors.white),),
+              ),
+              onPressed:() {
+                // TODO: Login form
+              },
+              )
+          ],
+        )
+        ),
     );
   }
 }
