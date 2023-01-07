29c35b348bdc9ed25a38d7ff4247d75d8fb6a124
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-01-06 22:47:11 -0600

    espacio entre tarjetas modificado

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -8,7 +8,7 @@ class ProductCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal:  20),
       child: Container(
-        margin: const EdgeInsets.only(top: 30, bottom: 50),
+        margin: const EdgeInsets.only(top: 30, bottom: 10),
         width: double.infinity,
         height: 400,
         decoration: _cardBorders(),
