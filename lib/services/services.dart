304187e9a58f313efaafee00a7217d4cfae41ae8
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-01-10 20:43:20 -0600

    NotificationsService

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,4 +1,4 @@
 
 export 'package:productos_app/services/auth_service.dart';
-
+export 'package:productos_app/services/notifications._service.dart';
 export 'package:productos_app/services/products_service.dart';
